8775e24a9d2dd57170b08767261899f3d01fb74e
Gavin <Gavin@users.noreply.github.com>
2015-03-24 21:33:04 -0500

    Initial commit

---
+++ app/src/test/java/com/republicofgavin/creditcardholder/activities/AddCreditCardActivityTest.java
@@ -0,0 +1,24 @@
+package com.republicofgavin.creditcardholder.activities;
+
+import org.robolectric.Robolectric;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by gavin on 3/24/15.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class AddCreditCardActivityTest {
+
+    @Before
+    public void setup(){
+
+    }
+    @Test
+    public void testSomething(){
+
+    }
+}
