7d6d7169010867bbad99c5de28b6ccfb5f165ccd
Gavin <Gavin@users.noreply.github.com>
2015-03-26 12:01:55 -0500

    Refactored enum to only contain type of company. Moved its digit code to the CreditCardValidator Wrote CreditCardValidator and unit tests for it.

--- app/src/main/java/com/republicofgavin/creditcardholder/util/CreditCardValidator.java
+++ app/src/main/java/com/republicofgavin/creditcardholder/util/CreditCardValidator.java
@@ -1,12 +1,141 @@
 package com.republicofgavin.creditcardholder.util;
 
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.republicofgavin.creditcardholder.datamodel.CreditCardCompanyType;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
- * Does validation of credit card data (credit card numbers, CVVs, and credit card types).
+ * Does validation of credit card data (credit card numbers, CVVs, expiration dates, and credit card types).
  *
  * @author Gavin McNeely
  */
 public class CreditCardValidator {
 
+    private static final String TAG = CreditCardValidator.class.getSimpleName();
 
+    /**
+     * Checks to see if the credit card number passes the Luhn formula test http://en.wikipedia.org/wiki/Luhn_algorithm).
+     *
+     * @param creditCardNumber String version of a number to run the algorithm on. If the input is: not a number, null, empty, or blank (ex:"   "). False is returned.
+     * @return True if the credit card number is valid. False otherwise.
+     */
+    public static boolean isLuhnValidated(final String creditCardNumber) {
+        if (creditCardNumber == null || TextUtils.isEmpty(creditCardNumber.trim()) || !TextUtils.isDigitsOnly(creditCardNumber)) {
+            return false;
+        }
+        int sum = 0;
+        //Iterate through the number doubling every other digit.
+        boolean shouldDouble = false;
+        for (int count = creditCardNumber.length() - 1; count >= 0; count--) {
+            int currentDigit = Character.getNumericValue(creditCardNumber.charAt(count));
+            //if we don't double the digit, we just add it to our total.
+            if (!shouldDouble) {
+                sum = sum + currentDigit;
+            }
+            // if we double our digit, check to see if it is 10 or greater. If not, just add the double to the sum otherwise add each digit
+            //of the doubled number to the sum.
+            else {
+                currentDigit = currentDigit * 2;
+                //At most the double of any single digit integer is 18(9*2), thus we can just always add one in the double digit case (the +1) The -10 removes the first digit in the number.
+                //Effectively, adding the first digit and the second digit together.
+                sum = sum + ((currentDigit <= 9) ? currentDigit
+                        : (currentDigit - 10) + 1);
+            }
+            shouldDouble = !shouldDouble;
+        }
+        return (sum % 10 == 0);
+    }
+
+    /**
+     * Checks the enum and the credit card number string to see if the credit card number has the correct number of digits. For example AMEX has only 15 digit CCNs while others can have 16 digits.
+     *
+     * @param creditCardCompanyType The {@link com.republicofgavin.creditcardholder.datamodel.CreditCardCompanyType} you are checking against.
+     * @param creditCardNumber      The String version of the CCN you are checking. If null, empty, blank, or not all numbers is passed in, it is always false.
+     * @return True if the CCN has the correct number of digits for the {@link com.republicofgavin.creditcardholder.datamodel.CreditCardCompanyType}
+     */
+    public static boolean isValidNumberOfCreditCardDigits(final CreditCardCompanyType creditCardCompanyType, final String creditCardNumber) {
+        if (creditCardNumber == null || TextUtils.isEmpty(creditCardNumber.trim()) || !TextUtils.isDigitsOnly(creditCardNumber)) {
+            Log.w(TAG, "Error: Invalid input given to isValidNumberOfCreditCardDigits. Returning false. creditCardCompanyType:" +
+                    creditCardCompanyType.name() + " creditCardNumber:" + creditCardNumber);
+            return false;
+        }
+        final int digitsInCard;
+        switch (creditCardCompanyType) {
+            case JCB:
+            case MASTERCARD:
+            case DISCOVER:
+            case VISA:
+            case UNKNOWN:
+                digitsInCard = 16;
+                break;
+            case AMEX:
+                digitsInCard = 15;
+                break;
+            default:
+                digitsInCard = -1;
+                Log.e(TAG, "Error: Invalid CreditCardCompanyType used for credit card digit verification. Returning false.");
+        }
+        return creditCardNumber.length() == digitsInCard;
+    }
+
+    /**
+     * Checks the enum and the CVV string to see if the CCV has the correct number of digits. For example AMEX has only 4 digit CCVs while others can have 3 digits.
+     *
+     * @param creditCardCompanyType The {@link com.republicofgavin.creditcardholder.datamodel.CreditCardCompanyType} you are checking against.
+     * @param cvvNumber             The String version of the CCV you are checking. If null, empty, blank, or not all numbers is passed in, it is always false.
+     * @return True if the CCV has the correct number of digits for the {@link com.republicofgavin.creditcardholder.datamodel.CreditCardCompanyType}
+     */
+    public static boolean isValidNumberOfCVVDigits(final CreditCardCompanyType creditCardCompanyType, final String cvvNumber) {
+        if (cvvNumber == null || TextUtils.isEmpty(cvvNumber.trim()) || !TextUtils.isDigitsOnly(cvvNumber)) {
+            Log.w(TAG, "Error: Invalid input given to isValidNumberOfCreditCardDigits. Returning false. creditCardCompanyType:" +
+                    creditCardCompanyType.name() + " cvvNumber:" + cvvNumber);
+            return false;
+        }
+        final int digitsInCVV;
+        switch (creditCardCompanyType) {
+            case JCB:
+            case MASTERCARD:
+            case DISCOVER:
+            case VISA:
+                digitsInCVV = 3;
+                break;
+            case AMEX:
+            case UNKNOWN:
+                digitsInCVV = 4;
+                break;
+            default:
+                digitsInCVV = -1;
+                Log.e(TAG, "Error: Invalid CreditCardCompanyType used for CCV. Returning false.");
+        }
+        return cvvNumber.length() == digitsInCVV;
+    }
+
+    /**
+     * Checks to see if the month and 2000+lastTwoDigitsInYear is expired based off of the date the JVM tells us. This is done by taking the expiration date and setting it
+     * to the first moment in the next month as it is month precise.
+     *
+     * @param month               The integer version of month(1-12). Other numbers are considered invalid and will make the method return true.
+     * @param lastTwoDigitsInYear The integer version of the last two digits of the year (00-99 00=0 01=1, and etc.). Other numbers are considered invalid and will make the method return true.
+     * @return True if any of the input is invalid or if the date is expired.
+     */
+    public static boolean isExpired(final int month, final int lastTwoDigitsInYear) {
+        if (month < 1 || month > 12 || lastTwoDigitsInYear < 0 || lastTwoDigitsInYear > 99) {
+            //Instead of throwing an IllegalArgumentException, we just return true indicating that the value is not good. As this is a validator and not a data-model.
+            return true;
+        }
+        //We check to see if it is before the first day hour, minute, second of next month. If so, it is still valid
+        final GregorianCalendar expirationDate = new GregorianCalendar();
+        expirationDate.set(2000 + lastTwoDigitsInYear, month, 1, 0, 0, 0);
+        expirationDate.add(GregorianCalendar.MONTH, 1);
+        //Consideration: Grab the date from a time server as the JVM's concept of time can be out of sync with reality. As in, the user does not have it
+        //auto-updating or just sets it incorrectly.
+        final GregorianCalendar today = new GregorianCalendar();
+        today.setTime(new Date());
+        return today.before(expirationDate);
+    }
 }
 
--- app/src/test/java/com/republicofgavin/creditcardholder/util/CreditCardValidatorTest.java
+++ app/src/test/java/com/republicofgavin/creditcardholder/util/CreditCardValidatorTest.java
@@ -1,7 +1,118 @@
 package com.republicofgavin.creditcardholder.util;
 
+import com.republicofgavin.creditcardholder.datamodel.CreditCardCompanyType;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
- * Created by gavin on 3/25/15.
+ * Tests {@link com.republicofgavin.creditcardholder.util.CreditCardValidator}
+ * @author Gavin McNeely
  */
+@RunWith(RobolectricTestRunner.class)
 public class CreditCardValidatorTest {
+
+    private final String validSixteenDigitCCNumber = "3379513561108795";
+    //Test input from email
+    private final String validAMEXCCNumber = "371449635398431";
+    private final String validDiscoverCCNumber = "6011111111111117";
+    private final String validJCBCCNumber = "3530111333300000";
+    private final String validMasterCardCCNumber = "5555555555554444";
+    private final String validVisaCCNumber = "4111111111111111";
+
+    private final String invalidCCNumberSpace = "33795135611 08795";
+    private final String invalidCCNumberNegative = "3379513561-108795";
+    private final String invalidSixteenDigitCCNumber = "3379513561108791";
+    private final String invalidFifteenDigitCCNumber = "337951356118791";
+
+    private final String invalidCCVNumberSpace = "4 44";
+    private final String invalidCCVNumberNegative = "44-4";
+
+    @Test
+    public void testIsLuhnValidated() {
+        Assert.assertFalse("null case was true", CreditCardValidator.isLuhnValidated(null));
+        Assert.assertFalse("empty case was true", CreditCardValidator.isLuhnValidated(""));
+        Assert.assertFalse("blank case was true", CreditCardValidator.isLuhnValidated(" "));
+        Assert.assertFalse("space in number case was true", CreditCardValidator.isLuhnValidated(invalidCCNumberSpace));
+        Assert.assertFalse("negative in number case was true", CreditCardValidator.isLuhnValidated(invalidCCNumberNegative));
+        Assert.assertFalse("non-zero check 16 digit sum case was true", CreditCardValidator.isLuhnValidated(invalidSixteenDigitCCNumber));
+        Assert.assertFalse("non-zero check 15 digit sum case was true", CreditCardValidator.isLuhnValidated(invalidFifteenDigitCCNumber));
+
+        Assert.assertTrue("generic 16 digit case was false", CreditCardValidator.isLuhnValidated(validSixteenDigitCCNumber));
+        Assert.assertTrue("valid AMEX case was false", CreditCardValidator.isLuhnValidated(validAMEXCCNumber));
+        Assert.assertTrue("valid Discover case was false", CreditCardValidator.isLuhnValidated(validDiscoverCCNumber));
+        Assert.assertTrue("valid JCB case was false", CreditCardValidator.isLuhnValidated(validJCBCCNumber));
+        Assert.assertTrue("valid MasterCard case was false", CreditCardValidator.isLuhnValidated(validMasterCardCCNumber));
+        Assert.assertTrue("valid Visa case was false", CreditCardValidator.isLuhnValidated(validVisaCCNumber));
+    }
+
+    @Test
+    public void testIsValidNumberOfCreditCardDigits() {
+        Assert.assertFalse("null case was true", CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.AMEX, null));
+        Assert.assertFalse("empty case was true", CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.DISCOVER, ""));
+        Assert.assertFalse("blank case was true", CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.JCB, " "));
+        Assert.assertFalse("space in number case was true", CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.MASTERCARD, invalidCCNumberSpace));
+        Assert.assertFalse("negative in number case was true", CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.UNKNOWN, invalidCCNumberNegative));
+
+        Assert.assertTrue("AMEX case was false", CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.AMEX, validAMEXCCNumber));
+        Assert.assertTrue("Discover case was false", CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.DISCOVER, validDiscoverCCNumber));
+        Assert.assertTrue("JCB case was false", CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.JCB, validJCBCCNumber));
+        Assert.assertTrue("MasterCard case was false", CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.MASTERCARD, validMasterCardCCNumber));
+        Assert.assertTrue("VISA case was false", CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.VISA, validVisaCCNumber));
+        //Assert.assertTrue("UNKNOWN case was false",CreditCardValidator.isValidNumberOfCreditCardDigits(CreditCardCompanyType.UNKNOWN,"00000000000000000000000000000000000000"));
+    }
+
+    @Test
+    public void testIsValidNumberOfCCVDigits() {
+        Assert.assertFalse("null CVV case was true", CreditCardValidator.isValidNumberOfCVVDigits(CreditCardCompanyType.AMEX, null));
+        Assert.assertFalse("empty CVV case was true", CreditCardValidator.isValidNumberOfCVVDigits(CreditCardCompanyType.DISCOVER, ""));
+        Assert.assertFalse("blank CVV case was true", CreditCardValidator.isValidNumberOfCVVDigits(CreditCardCompanyType.JCB, " "));
+        Assert.assertFalse("space in number CVV case was true", CreditCardValidator.isValidNumberOfCVVDigits(CreditCardCompanyType.MASTERCARD, invalidCCVNumberSpace));
+        Assert.assertFalse("negative in number CVV case was true", CreditCardValidator.isValidNumberOfCVVDigits(CreditCardCompanyType.UNKNOWN, invalidCCVNumberNegative));
+
+        Assert.assertTrue("AMEX CVV case was false", CreditCardValidator.isValidNumberOfCVVDigits(CreditCardCompanyType.AMEX, "4444"));
+        Assert.assertTrue("Discover CCV case was false", CreditCardValidator.isValidNumberOfCVVDigits(CreditCardCompanyType.DISCOVER, "444"));
+        Assert.assertTrue("JCB CVV case was false", CreditCardValidator.isValidNumberOfCVVDigits(CreditCardCompanyType.JCB, "444"));
+        Assert.assertTrue("MasterCard CVV case was false", CreditCardValidator.isValidNumberOfCVVDigits(CreditCardCompanyType.MASTERCARD, "444"));
+        Assert.assertTrue("VISA CVV case was false", CreditCardValidator.isValidNumberOfCVVDigits(CreditCardCompanyType.VISA, "444"));
+        //Assert.assertTrue("UNKNOWN CCV case was false",CreditCardValidator.isValidNumberOfCCVDigits(CreditCardCompanyType.UNKNOWN,"00000000000000000000000000000000000000"));
+    }
+
+    @Test
+    public void testIsExpired() {
+        Assert.assertTrue("negative month case was false", CreditCardValidator.isExpired(-1, 99));
+        Assert.assertTrue("month overflow case was false", CreditCardValidator.isExpired(13, 99));
+        Assert.assertTrue("negative year case was false", CreditCardValidator.isExpired(1, -1));
+        Assert.assertTrue("year overflow case was false", CreditCardValidator.isExpired(1, 100));
+
+        Assert.assertTrue("December 2099 case was false", CreditCardValidator.isExpired(12, 99));
+
+        final GregorianCalendar lastMonth = new GregorianCalendar();
+        lastMonth.setTime(new Date());
+        lastMonth.add(GregorianCalendar.MONTH, -1);
+        Assert.assertFalse("last month case was true", CreditCardValidator.isExpired(lastMonth.get(GregorianCalendar.MONTH), lastMonth.get(GregorianCalendar.YEAR) - 2000));
+
+        final GregorianCalendar lastYear = new GregorianCalendar();
+        lastYear.setTime(new Date());
+        lastYear.add(GregorianCalendar.YEAR, -1);
+        lastYear.add(GregorianCalendar.MONTH, 1);
+        Assert.assertFalse("last year case was true", CreditCardValidator.isExpired(lastYear.get(GregorianCalendar.MONTH), lastYear.get(GregorianCalendar.YEAR) - 2000));
+
+        final GregorianCalendar today = new GregorianCalendar();
+        today.setTime(new Date());
+        Assert.assertTrue("today case was false", CreditCardValidator.isExpired(today.get(GregorianCalendar.MONTH), today.get(GregorianCalendar.YEAR) - 2000));
+
+        final GregorianCalendar nextYear = new GregorianCalendar();
+        nextYear.setTime(new Date());
+        lastYear.add(GregorianCalendar.YEAR, 1);
+        lastYear.add(GregorianCalendar.MONTH, -1);
+        Assert.assertTrue("nextYear case was false", CreditCardValidator.isExpired(nextYear.get(GregorianCalendar.MONTH), nextYear.get(GregorianCalendar.YEAR) - 2000));
+    }
+
 }
