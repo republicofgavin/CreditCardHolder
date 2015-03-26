aefa374276f5f281b2c6fc1c5eb36ba5016b6902
Gavin <Gavin@users.noreply.github.com>
2015-03-26 16:45:19 -0500

    Created basic layout design for portrait view. Needs to be themed and cleaned up.

---
+++ app/src/main/java/com/republicofgavin/creditcardholder/com/republicofgavin/creditcardholder/fragments/AddCreditCardFragment.java
@@ -0,0 +1,9 @@
+package com.republicofgavin.creditcardholder.com.republicofgavin.creditcardholder.fragments;
+
+/**
+ * Displays a form to enter credit card information.
+ *
+ * @author Gavin McNeely
+ */
+public class AddCreditCardFragment {
+}
