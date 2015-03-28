47a617a4046c2f542e1429437fa474e4835ccf22
Gavin <Gavin@users.noreply.github.com>
2015-03-27 20:57:11 -0500

    Added home button for easier leaving of the app/activity

--- app/src/main/java/com/republicofgavin/creditcardholder/activities/AddCreditCardActivity.java
+++ app/src/main/java/com/republicofgavin/creditcardholder/activities/AddCreditCardActivity.java
@@ -1,14 +1,22 @@
 package com.republicofgavin.creditcardholder.activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.republicofgavin.creditcardholder.R;
 
-
+/**
+ * Activity for holding the {@link com.republicofgavin.creditcardholder.fragments.AddCreditCardFragment}
+ *
+ * @author Gavin McNeely
+ */
 public class AddCreditCardActivity extends Activity {
+
+    private static final String TAG = AddCreditCardActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,26 +24,22 @@ public class AddCreditCardActivity extends Activity {
         setContentView(R.layout.activity_add_credit_card);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected");
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_add_credit_card, menu);
+        getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_HOME_AS_UP);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
